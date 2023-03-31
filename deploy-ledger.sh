066621765206132efbdd3881f05df73c90752e67
Moritz <fullermoritz@gmail.com>
2023-03-31 10:30:52 +0200

    🩹 fixed vscode bug

--- deploy-ledger.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cat <<<"$(jq '.canisters.ledger.candid="./declarations/ledger/ledger.private.did"' dfx.json)" >dfx.json
-npm run deploy:ledger-wasm
-cat <<<"$(jq '.canisters.ledger.candid="./declarations/ledger/ledger.public.did"' dfx.json)" >dfx.json
