1911118e51be7f36cea347197deab613aa32bd7b
Zen Voich <zen.voich@gmail.com>
2023-04-11 15:17:47 +0400

    fixed deploy from tests

--- test/setup.ts
+++ test/setup.ts
@@ -3,18 +3,17 @@ import {execSync} from 'child_process';
 import {beforeAll} from 'vitest';
 import {applyEnv} from './apply-env';
 
-let prevEnvName = '';
-
 beforeAll(async (suite) => {
   let envName = path.dirname(suite.name).split('/').at(-1);
 
-  if (envName !== prevEnvName) {
-    prevEnvName = envName;
+  if (envName !== globalThis.prevEnvName) {
+    globalThis.prevEnvName = envName;
 
     await applyEnv(envName);
 
     execSync(`npm run deploy-test`, {
-      cwd: path.resolve(__dirname, '..')
+      cwd: path.resolve(__dirname, '..'),
+      stdio: ['ignore', 'ignore', 'pipe'],
     });
 
     if (envName === 'multi-asset') {
