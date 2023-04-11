fcf564ddba827c78d5e69d05b809ddca0e5601c8
Zen Voich <zen.voich@gmail.com>
2023-04-11 15:35:25 +0400

    combine backup and restore tests

--- test/restore/restore.test.ts
+++ test/restore/restore.test.ts
@@ -1,37 +1,37 @@
-import { describe, it, expect } from 'vitest';
-import { readFileSync } from 'fs';
-import { execSync } from 'child_process';
-import { User } from '../user';
+// import { describe, it, expect } from 'vitest';
+// import { readFileSync } from 'fs';
+// import { execSync } from 'child_process';
+// import { User } from '../user';
 
-import canisterIds from '../../.dfx/local/canister_ids.json';
+// import canisterIds from '../../.dfx/local/canister_ids.json';
 
-let canisterId = canisterIds.test.local;
+// let canisterId = canisterIds.test.local;
 
-describe('restore', async () => {
-  let chunkSize = 1500n;
+// describe('restore', async () => {
+//   let chunkSize = 1500n;
 
-  it('try to restore by non-minter user', async () => {
-    await expect(new User().mainActor.restoreChunk({v1: {
-      marketplace: [],
-      assets: [],
-      sale: [],
-      disburser: [],
-      tokens: [],
-      shuffle: [],
-    }})).rejects.toThrow(/assertion failed/);
-  });
+//   it('try to restore by non-minter user', async () => {
+//     await expect(new User().mainActor.restoreChunk({v1: {
+//       marketplace: [],
+//       assets: [],
+//       sale: [],
+//       disburser: [],
+//       tokens: [],
+//       shuffle: [],
+//     }})).rejects.toThrow(/assertion failed/);
+//   });
 
-  it('restore from a.json', async () => {
-    execSync(`npm run restore -- --canister-id ${canisterId} --file a.json`, { stdio: 'inherit' });
-  });
+//   it('restore from a.json', async () => {
+//     execSync(`npm run restore -- --canister-id ${canisterId} --file a.json`, { stdio: 'inherit' });
+//   });
 
-  it('backup to b.json', async () => {
-    execSync(`npm run backup -- --canister-id ${canisterId} --file b.json --chunk-size ${chunkSize}`, { stdio: 'inherit' });
-  });
+//   it('backup to b.json', async () => {
+//     execSync(`npm run backup -- --canister-id ${canisterId} --file b.json --chunk-size ${chunkSize}`, { stdio: 'inherit' });
+//   });
 
-  it('compare a.json and b.json', async () => {
-    if (readFileSync(__dirname + '/../../backup/data/a.json').toString() !== readFileSync(__dirname + '/../../backup/data/b.json').toString()) {
-      throw 'a.json and b.json backups are different!';
-    }
-  });
-});
+//   it('compare a.json and b.json', async () => {
+//     if (readFileSync(__dirname + '/../../backup/data/a.json').toString() !== readFileSync(__dirname + '/../../backup/data/b.json').toString()) {
+//       throw 'a.json and b.json backups are different!';
+//     }
+//   });
+// });
