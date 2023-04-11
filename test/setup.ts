febfd8ad924afb3bf06a63dad82c43ae8eaddce3
Zen Voich <zen.voich@gmail.com>
2023-04-11 13:50:30 +0400

    fixed deploy from tests

--- test/setup.ts
+++ test/setup.ts
@@ -10,6 +10,17 @@ beforeAll((suite) => {
   if (envName !== prevEnvName) {
     applyEnv(envName);
     execSync(`npm run deploy-test`);
+
+    if (envName === 'multi-asset') {
+      execSync(`npm run mint:test-2`);
+    }
+    else if (envName === 'single-asset-delayed-reveal') {
+      execSync(`npm run mint:test-2`);
+      execSync(`dfx canister call test shuffleTokensForSale`);
+    }
+    else {
+      execSync(`npm run mint:test`);
+    }
   }
   prevEnvName = envName;
 });
