c2233625b94c13c63187ebe849aed9a87cabe377
Zen Voich <zen.voich@gmail.com>
2023-04-11 17:01:31 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
   test: {
     testTimeout: 1000 * 60 * 2,
     // setupFiles: ['./test/setup.ts'],
-    // exclude: [
-    //   '**/node_modules/**',
-    //   '**/.{git,dfx,vessel}/**',
-    //   'test/marketplace',
-    //   'test/backup-restore',
-    // ],
+    exclude: [
+      '**/node_modules/**',
+      '**/.{git,dfx,vessel}/**',
+      // 'test/marketplace',
+      'test/backup-restore',
+    ],
   },
 });
