3e5f80bf2689c42aa0dd3054897c7651f1c84984
ZenVoich <zen.voich@gmail.com>
2023-04-16 10:13:05 +0400

    fixed open edition test

--- test/open-edition/.env.open-edition.ts
+++ test/open-edition/.env.open-edition.ts
@@ -2,7 +2,7 @@ import defaultEnv from '../.env.default';
 
 export default {
   ...defaultEnv,
-  sale: `variant { duration = variant { seconds = 30 } }`,
+  sale: `variant { duration = variant { seconds = 50 } }`,
   revealDelay: 0n,
   singleAssetCollection: true,
 };
