373a927e5af1dc9285650e4f0aa27b2a058da745
Moritz Fuller <moritz.fuller@dfinity.org>
2023-07-28 12:19:33 -0400

    ✨ updated sale stats

--- backup/ethflowerSale.ts
+++ backup/ethflowerSale.ts
@@ -1,53 +1,23 @@
-import { SaleTransaction } from "../declarations/ethflower/ethflower.did";
 import { StableChunk__4 } from "../declarations/main/staging.did";
-import { getActor } from "./ethflowerActor";
-let mainActor = getActor("ic");
 
 export async function sale() {
-  const saleTransactions = await getSaleTransactions();
-
   const sale: StableChunk__4 = [
     {
       v2: {
         salesSettlements: [],
-        totalToSell: getSold(saleTransactions),
+        totalToSell: 0n,
         failedSales: [],
-        sold: getSold(saleTransactions),
-        saleTransactionChunk: saleTransactions,
-        saleTransactionCount: getSaleTransactionCount(saleTransactions),
+        sold: 0n,
+        saleTransactionChunk: [],
+        saleTransactionCount: 0n,
         nextSubAccount: 0n,
-        soldIcp: getSoldIcp(saleTransactions),
+        soldIcp: 0n,
         whitelistSpots: [],
         tokensForSale: [],
       },
     },
   ];
   return sale;
-}
-
-async function getSaleTransactions() {
-  const saleTransactions = await mainActor.saleTransactions();
-  return saleTransactions;
-}
-
-function getSoldIcp(saleTransactions: SaleTransaction[]) {
-  let soldIcp = BigInt(0);
-  saleTransactions.forEach((saleTransaction) => {
-    soldIcp += saleTransaction.price;
-  });
-  return soldIcp;
-}
-
-function getSold(saleTransactions: SaleTransaction[]) {
-  let sold = BigInt(0);
-  saleTransactions.forEach((saleTransaction) => {
-    sold += BigInt(saleTransaction.tokens.length);
-  });
-  return sold;
-}
-
-function getSaleTransactionCount(saleTransactions: SaleTransaction[]) {
-  return BigInt(saleTransactions.length);
 }
 
 sale();
