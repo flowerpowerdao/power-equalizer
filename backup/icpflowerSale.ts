b720aacea73be194eba1b06d8a4d50a4a1c4762c
Moritz Fuller <moritz.fuller@dfinity.org>
2023-09-01 10:09:24 +0200

    ✨ implemnted sale

--- backup/icpflowerSale.ts
+++ backup/icpflowerSale.ts
@@ -1,22 +1,22 @@
-// import { SaleTransaction } from "../declarations/btcflower/btcflower.did";
+import { SaleTransaction } from "../declarations/icpflower/icpflower.did";
 import { StableChunk__4 } from "../declarations/main/staging.did";
 import { getActor } from "./icpflowerActor";
 let mainActor = getActor("ic");
 
 export async function sale() {
-  // const saleTransactions = await getSaleTransactions();
+  const saleTransactions = await getSaleTransactions();
 
   const sale: StableChunk__4 = [
     {
       v2: {
         salesSettlements: [],
-        totalToSell: 0n,
+        totalToSell: getSold(saleTransactions),
         failedSales: [],
-        sold: 0n,
-        saleTransactionChunk: [],
-        saleTransactionCount: 0n,
+        sold: getSold(saleTransactions),
+        saleTransactionChunk: saleTransactions,
+        saleTransactionCount: getSaleTransactionCount(saleTransactions),
         nextSubAccount: 0n,
-        soldIcp: 0n,
+        soldIcp: getSoldIcp(saleTransactions),
         whitelistSpots: [],
         tokensForSale: [],
       },
@@ -25,29 +25,29 @@ export async function sale() {
   return sale;
 }
 
-// async function getSaleTransactions() {
-//   const saleTransactions = await mainActor.saleTransactions();
-//   return saleTransactions;
-// }
+async function getSaleTransactions() {
+  const saleTransactions = await mainActor.saleTransactions();
+  return saleTransactions;
+}
 
-// function getSoldIcp(saleTransactions: SaleTransaction[]) {
-//   let soldIcp = BigInt(0);
-//   saleTransactions.forEach((saleTransaction) => {
-//     soldIcp += saleTransaction.price;
-//   });
-//   return soldIcp;
-// }
+function getSoldIcp(saleTransactions: SaleTransaction[]) {
+  let soldIcp = BigInt(0);
+  saleTransactions.forEach((saleTransaction) => {
+    soldIcp += saleTransaction.price;
+  });
+  return soldIcp;
+}
 
-// function getSold(saleTransactions: SaleTransaction[]) {
-//   let sold = BigInt(0);
-//   saleTransactions.forEach((saleTransaction) => {
-//     sold += BigInt(saleTransaction.tokens.length);
-//   });
-//   return sold;
-// }
+function getSold(saleTransactions: SaleTransaction[]) {
+  let sold = BigInt(0);
+  saleTransactions.forEach((saleTransaction) => {
+    sold += BigInt(saleTransaction.tokens.length);
+  });
+  return sold;
+}
 
-// function getSaleTransactionCount(saleTransactions: SaleTransaction[]) {
-//   return BigInt(saleTransactions.length);
-// }
+function getSaleTransactionCount(saleTransactions: SaleTransaction[]) {
+  return BigInt(saleTransactions.length);
+}
 
 sale();
